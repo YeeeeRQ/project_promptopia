1b67137de86aa8551b8be6f6df1decce8839eb39
YeeeeRQ <YeeeeRQ@outlook.com>
2023-08-11 21:41:29 +0800

    create prompt form

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -5,13 +5,36 @@ import { useRouter } from "next/navigation";
 import Form from "@components/Form";
 
 const CreatePrompt = () => {
+  const router = useRouter();
+  const { data: session } = useSession();
   const [submitting, setSubmitting] = useState(false);
   const [post, setPost] = useState({
     prompt: "",
     tag: "",
   });
 
-  const CreatePrompt = async (e) => {};
+  const CreatePrompt = async (e) => {
+    e.preventDefault();
+    setSubmitting(true);
+
+    try {
+      const response = await fetch("/api/prompt/new", {
+        method: "POST",
+        body: JSON.stringify({
+          userId: session?.user.id,
+          prompt: post.prompt,
+          tag: post.tag,
+        }),
+      });
+      if (response.ok) {
+        router.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setSubmitting(false);
+    }
+  };
 
   return (
     <Form
