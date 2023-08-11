9ee7aa5d688b13a35ecdd8eeedf30ab952664829
YeeeeRQ <YeeeeRQ@outlook.com>
2023-08-11 18:47:53 +0800

    OAuth Login

--- utils/database.js
+++ utils/database.js
@@ -5,13 +5,13 @@ let isConnected = false; // track the connection
 export const connectToDB = async () => {
   mongoose.set("strictQuery", true);
 
-  if (isconnected) {
+  if (isConnected) {
     console.log("MongoDB is already connected");
     return;
   }
 
   try {
-    await mongoose.connect(process.env.MONGODB_RUI, {
+    await mongoose.connect(process.env.MONGODB_URI, {
       dbName: "share_prompt",
       useNewUrlParser: true,
       useUnifiedTopology: true,
